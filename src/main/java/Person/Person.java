a56a149bb9df1de643d34578cff1e1c4802bc855
Lukasz Labusek <lukasz.labusek@gmail.com>
2020-02-19 19:47:38 +0100

    little clean with usage of Uncle's Bob directions

--- src/main/java/Person/Person.java
+++ src/main/java/Person/Person.java
@@ -10,31 +10,18 @@ public class Person {
         this.lastName = lastName;
         this.age = age;
     }
-    Person(){}
-
 
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
     public int getAge() {
         return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     @Override
