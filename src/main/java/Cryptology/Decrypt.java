dc1e0f8c75720865f0907493a89e106c34aba610
luklab92 <58621248+luklab92@users.noreply.github.com>
2020-02-21 14:41:50 +0100

    Update Decrypt.java
    update

--- src/main/java/Cryptology/Decrypt.java
+++ src/main/java/Cryptology/Decrypt.java
@@ -1,60 +1,54 @@
 package Cryptology;
 
 public class Decrypt {
+    private final int big = 'A';
+    private final int small = 'a';
+    private final int backToStart = 'z' - 'a' + 1;
+    ;
 
-        private String input;
-        public Decrypt(String secret, int jump) {
-            this.input = secret;
-            finish(jump);
+
+    private String[] split(String input, int jump) {
+        boolean isBig;
+        String[] output = new String[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != ' ') {
+                int value = input.charAt(i);
+                isBig = isItBigLetter(value);
+                value = value - jump;
+                if (!isBig)
+                    value = smallLetterValueCheck(value);
+                else
+                    value = bigLetterValueCheck(value);
+                String s = Character.toString((char) value);
+                output[i] = s;
+            } else output[i] = String.valueOf(input.charAt(i));
         }
-
-        private String[] split(int jump) {
-            boolean isBig = false;
-            String[] output = new String[input.length()];
-            for (int i = 0; i <input.length(); i++) {
-                if (input.charAt(i) != ' ') {
-                    int value = input.charAt(i);
-
-                    if (isItBigLetter(value)) isBig=true;
-                    value = value - jump;
-
-                    if (!isBig)
-                        value = smallLetterValueCheck(value);
-
-                    if (isBig)
-                        value = bigLetterValueCheck(value);
-
-                    String s = Character.toString((char) value);
-                    output[i] = s;
-                }
-                else output[i] = String.valueOf(input.charAt(i));
-            }
-            return output;
-        }
-
-        private void finish(int jump) {
-            String[] string = split(jump);
-            StringBuilder sb = new StringBuilder();
-            for (String s: string) {
-                sb.append(s);
-            }
-            System.out.println(sb.toString());
-        }
-
-        private int bigLetterValueCheck(int value) {
-
-            if (value<65) value=value+26;
-            return value;
-        }
-
-        private int smallLetterValueCheck(int value) {
-            if (value<97) value=value+26;
-            return value;
-        }
-
-        private boolean isItBigLetter (int value) {
-            return (value<95) ;
-        }
-
+        return output;
     }
 
+
+    public String finish(String input, int jump) {
+        String[] string = split(input, jump);
+        StringBuilder sb = new StringBuilder();
+        for (String s : string) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
+    private int bigLetterValueCheck(int value) {
+
+        if (value < big) value = value + backToStart;
+        return value;
+    }
+
+    private int smallLetterValueCheck(int value) {
+        if (value < small) value = value + backToStart;
+        return value;
+    }
+
+    private boolean isItBigLetter(int value) {
+        return (value < small);
+    }
+
+}
