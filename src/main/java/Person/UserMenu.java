bf9164cff33ac8ada68d5d07b72807b28a1a1c4a
luklab92 <58621248+luklab92@users.noreply.github.com>
2020-02-21 12:59:12 +0100

    Update UserMenu.java

--- src/main/java/Person/UserMenu.java
+++ src/main/java/Person/UserMenu.java
@@ -1,6 +1,8 @@
 package Person;
 
 
+
+
 import java.io.*;
 import java.util.*;
 
@@ -9,80 +11,90 @@ public class UserMenu {
     private Scanner scanner = new Scanner(System.in);
     private List<Person> employeeList = new ArrayList<>();
     private SortType type;
+    private final int error = -1;
 
     public UserMenu() {
         askForEmployee();
     }
-    private void askForEmployee(){
+
+    private void askForEmployee() {
         boolean flag = false;
-            while (!flag) {
-                try {
-                    scanner = new Scanner(System.in);
-                    System.out.println("Type number of employees");
-                    numberOfEmployees = scanner.nextInt();
-                }
-             catch(InputMismatchException e){
-                    System.out.println("YOU MUST TYPE A NUMBER!!! \n");
-                    numberOfEmployees = -1;
-                }
-                if (numberOfEmployees != -1) {
-                    addPerson();
-                    sortEmployersByLastName();
-                    print();
-                    saveToFile();
-                    flag = true;
-                }
+        while (!flag) {
+                scanner = new Scanner(System.in);
+                System.out.println("Type number of employees");
+                numberOfEmployees = userIntChoice();
+            if (numberOfEmployees != error) {
+                addPerson();
+                flag = true;
             }
         }
-    private void addPerson(){
+        sortEmployersByLastName();
+        print();
+        saveToFile();
+        scanner.close();
+    }
+
+    private void addPerson() {
         int counter = 0;
         String firstName;
         String lastName;
         int age;
-        while (counter<numberOfEmployees){
-                System.out.println("Type firstName");
-                scanner.nextLine();
-                firstName = scanner.nextLine();
-                System.out.println("Type secondName");
-                lastName = scanner.nextLine();
-            try {
+        while (counter < numberOfEmployees) {
+            System.out.println("Type firstName");
+            scanner.nextLine();
+            firstName = userStringChoice();
+            System.out.println("Type secondName");
+            lastName = userStringChoice();
                 System.out.println("Type age");
-                age = scanner.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("YOU MUST TYPE A NUMBER!!! \n");
-                age=-1;
-            }
-
-            if (age!=-1) {
+                age = userIntChoice();
+            if (age != error) {
                 employeeList.add(new Person(firstName, lastName, age));
                 counter++;
             }
         }
     }
 
-    private void sortEmployersByLastName(){
 
-        int choose=-1;
-        while (choose<0 || choose>2) {
+    private int userIntChoice() {
+        int result;
+        try {
+            result = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("YOU MUST TYPE A NUMBER!!! \n");
+            return error;
+        }
+        return result;
+    }
+
+    private String userStringChoice() {
+        return scanner.nextLine();
+    }
+
+    private void sortEmployersByLastName() {
+
+        int choose = error;
+        while (choose < 0 || choose > 2) {
             sortChooseMenu();
-            choose = scanner.nextInt();
+            choose = userIntChoice();
         }
         scanner.close();
-            switch (choose) {
-                case 0:
-                    type = SortType.FIRSTNAME;
-                case 1:
-                    type = SortType.LASTNAME;
-                case 2:
-                    type = SortType.AGE;
-            }
+        switch (choose) {
+            case 0:
+                type = SortType.FIRSTNAME;
+            case 1:
+                type = SortType.LASTNAME;
+            case 2:
+                type = SortType.AGE;
+        }
         employeeList.sort(new SortAlgorithm(type));
     }
-    private void print(){
-        for (Person p: employeeList) {
+
+    private void print() {
+        for (Person p : employeeList) {
             System.out.println(p.toString());
         }
     }
+
     private void sortChooseMenu() {
         System.out.println("Choose sort type");
         System.out.println("0 - FirstName");
@@ -90,13 +102,13 @@ public class UserMenu {
         System.out.println("2 - Age");
     }
 
-    private void saveToFile(){
+    private void saveToFile() {
         try {
             PrintWriter pw = new PrintWriter(new FileOutputStream("baza.csv"));
             for (Person p : employeeList)
                 pw.println(p.toString());
             pw.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.getStackTrace();
         }
     }
